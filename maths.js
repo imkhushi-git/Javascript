f3e1df69f34c91b7dc9d5193e639b25434ce93f9
Khushi <khusipoudel3@gmail.com>
2026-03-18 14:58:38 +0000

    commit

---
+++ maths.js
@@ -0,0 +1,7 @@
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.2));
+console.log(Math.min(5,7,2,8));
+console.log(Math.max(5,7,2,8));
